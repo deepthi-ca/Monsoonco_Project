731630430049135c74c048dd6a17cf0fc95b884c
deepthi-ca <deepthiik@gmail.com>
2014-04-27 08:33:43 -0700

    Committing Constants

---
+++ Monsoonco/Classes/Constants.h
@@ -0,0 +1,14 @@
+//
+//  Constants.h
+//  Monsoonco
+//
+//  Created by Deepthi Muthyala on 4/26/14.
+//  Copyright (c) 2014 Sahasra. All rights reserved.
+//
+
+
+
+#define ANIMATION_ENABLED   YES
+
+
+
